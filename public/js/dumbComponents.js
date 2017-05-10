f8e3c0c80189e639bf850080be7058d71b0eb226
GaganTut <gstut@live.com>
2017-05-09 19:48:36 -1000

    Need to refactor it

---
+++ public/js/dumbComponents.js
@@ -0,0 +1,19 @@
+/*const dumbComponents = (() => {
+  const kanbanCard = ({title, priority, status, createdBy, assignedTo}) => (
+    <div>
+      <h1>{title}</h1>
+      <p>
+        Priority: {priority} <br/>
+        Status: {status} <br/>
+        By: {createdBy} <br/>
+        For: {assignedTo}
+      </p>
+    </div>
+  )
+
+  return {
+    kanbanCard
+  }
+})
+
+export default dumbComponents;*/
